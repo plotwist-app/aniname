d00ee03fbb54a846c8d74760662531bdc9f756af
João Pedro Alves <joaopealves.dev@gmail.com>
2024-04-19 01:22:09 -0300

    chore: turn on open ai call feature flag

--- api/web/guessController.ts
+++ api/web/guessController.ts
@@ -1,30 +1,29 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
-// import { guessAnimeService } from '../domain/services/guessAnimeService.js'
-// import { Message } from '../@types/message.js'
+import { guessAnimeService } from '../domain/services/guessAnimeService.js'
+import { Message } from '../@types/message.js'
 
-// interface BodyParams {
-//   messages: Message[]
-// }
+interface BodyParams {
+  messages: Message[]
+}
 
 export async function guessController(req: VercelRequest, res: VercelResponse) {
-  // const body: BodyParams = req.body
+  const body: BodyParams = req.body
 
-  // const response = await guessAnimeService(body.messages)
+  const response = await guessAnimeService(body.messages)
 
-  // if (!body.messages) {
-  //   res.status(422).json({
-  //     message:
-  //       'There are missing message, you need to pass an message to aniname',
-  //   })
+  if (!body.messages) {
+    res.status(422).json({
+      message:
+        'There are missing messages, you need to pass an message to aniname',
+    })
 
-  //   return
-  // }
+    return
+  }
 
   res.setHeader('Content-Type', 'application/json')
 
   res.status(200).json({
-    message:
-      "The anime you are referring to is The Genius Prince's Guide to Raising a Nation Out of Debt (Tensai Ouji no Akaji Kokka Saisei Jutsu in Japanese). The story follows Prince Wein, who is intelligent and capable but wishes to sell his impoverished kingdom to live a leisurely life. His plans are often complicated by unexpected events and the responsibilities of leadership. His aid and consort in his endeavors is Ninym, his white-haired childhood friend and advisor.",
+    message: response,
   })
 }
