40f6e595d8762d16e538f014d2b191316d89af63
João Pedro Alves <joaopealves.dev@gmail.com>
2024-04-28 14:35:57 -0300

    Fix  normalize response (#3)
    * chore: update version and normalize version
    
    * chore: add api key
    
    * chore: add api key env var

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 1.2.0
+
+- Add api key auth (#003)
+
 ## 1.1.1
 
 ### Fixed
--- api/web/guessController.ts
+++ api/web/guessController.ts
@@ -2,23 +2,38 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 
 import { guessAnimeService } from '../domain/services/guessAnimeService.js'
 import { Message } from '../@types/message.js'
+import { IncomingHttpHeaders } from 'http'
 
 interface BodyParams {
   messages: Message[]
 }
 
+const apiKeyEnv = process.env.API_KEY
+
 export async function guessController(req: VercelRequest, res: VercelResponse) {
   const body: BodyParams = req.body
+
+  const headers: IncomingHttpHeaders = req.headers
+
+  if (!headers.api_key) {
+    return res.status(401).json({
+      message: 'No api Key provided',
+    })
+  }
+
+  if (headers.api_key !== apiKeyEnv) {
+    return res.status(401).json({
+      message: 'Wrong API key',
+    })
+  }
 
   const response = await guessAnimeService(body.messages)
 
   if (!body.messages) {
-    res.status(422).json({
+    return res.status(422).json({
       message:
         'There are missing messages, you need to pass an message to aniname',
     })
-
-    return
   }
 
   res.status(200).json({
